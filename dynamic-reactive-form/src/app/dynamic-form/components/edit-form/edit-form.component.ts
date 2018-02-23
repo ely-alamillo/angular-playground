d8ad23e026aa8eb5b43731682d2d20b26e4420f9
piq9117 <piq9117@gmail.com>
2018-02-22 21:17:13 -0600

    correcting code from tutorial

--- dynamic-reactive-form/src/app/dynamic-form/components/edit-form/edit-form.component.ts
+++ dynamic-reactive-form/src/app/dynamic-form/components/edit-form/edit-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
 
 @Component({
@@ -6,4 +6,20 @@ import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
   templateUrl: "./edit-form.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EditForm {}
+export class EditForm implements OnInit {
+  public personEditForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ){ }
+
+  ngOnInit() {
+    this.personEditForm = this.initForm();
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      name: [""]
+    })
+  }
+}
--- dynamic-reactive-form/src/app/dynamic-form/dynamic-form.component.ts
+++ dynamic-reactive-form/src/app/dynamic-form/dynamic-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnChanges } from "@angular/core";
+import { FormGroup, FormControl, Validators, ValidatorFn } from "@angular/forms";
+import { concat, keys, map } from "sanctuary";
 
 @Component({
   selector: "dynamic-form",
@@ -7,7 +9,30 @@ import { Component, Input, OnChanges } from "@angular/core";
 export class DynamicForm implements OnChanges {
   @Input() model: any;
 
+  public formModel: any;
+  public form: FormGroup;
+
   ngOnChanges() {
-    console.table(this.model);
+    this.formModel = map((k: string) =>
+      concat({keys: k})(this.model[k]))(keys(this.model));
+    this.initForm();
+  }
+
+  initForm(): FormGroup {
+    const validateFormCtrl = (v: string) => this.isNil(v) ? "" : v;
+
+    return new FormGroup(keys(this.model)
+      .map(k => ({[k]: new FormControl(validateFormCtrl(this.model.value), this.mapValidators(this.model[k].validation))}))
+      .reduceRight((acc, v) => concat(v) (acc), {}));
+  }
+
+  isNil<N>(n?: N): boolean {
+    return n === undefined || n === null;
+  }
+
+  mapValidators(v: any): ValidatorFn[] {
+    const { required, min } = Validators;
+
+    return Object.keys(this.isNil(v) ? [] : v).map(k => k === "required" ? required : min(v[k]))
   }
 }
